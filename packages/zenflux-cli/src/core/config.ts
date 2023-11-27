ac3a297c5f7fa1d81e37f33516cf27bb0cd28513
Leonid Vinikov <czf.leo123@gmail.com>
2023-11-27 15:00:58 +0200

    Tweak:  (`cli`) - Config prepare for multi config support in same file

--- packages/zenflux-cli/src/core/config.ts
+++ packages/zenflux-cli/src/core/config.ts
@@ -5,8 +5,40 @@ import fs from "fs";
 
 import { console } from "@zenflux/cli/src/modules/console";
 
-import type { IZConfig } from "@zenflux/cli/src/definitions/config";
+import { Z_CONFIG_REQUIRED_KEYS } from "@zenflux/cli/src/definitions/config";
 
+import type { IZConfigInternal , IConfigArgsBase, IZConfigs, TConfigType} from "@zenflux/cli/src/definitions/config";
+
+type KeyOfZConfigRequiredKeys = keyof typeof Z_CONFIG_REQUIRED_KEYS;
+
+function hasRequiredKeys( config: IZConfigInternal ) {
+    return Z_CONFIG_REQUIRED_KEYS.every( ( key ) => {
+        return config.hasOwnProperty( key as KeyOfZConfigRequiredKeys );
+    } );
+}
+
+function configValidate( config: IZConfigInternal ) {
+    if ( config.inputDtsPath && ! config.outputDtsPath ) {
+        throw new Error( `'${ config.path }' inputDtsPath is defined but outputDtsPath is not` );
+    }
+}
+
+function configEnsureInternals( config: IZConfigInternal, args: {
+    type: TConfigType,
+    path: string,
+    name: string,
+    $defaults?: IConfigArgsBase,
+} ) {
+    Object.assign(
+        config as IZConfigInternal,
+        args.$defaults || {},
+        {
+            type: args.type,
+            path: args.path,
+            outputName: args.name,
+        }
+    );
+}
 export async function zConfigLoad( path: string, silent = false ) {
     // Check if target config exists.
     console.verbose( () => `${ zConfigLoad.name }() -> Checking if exists: '${ path }'` );
@@ -30,11 +62,65 @@ export async function zConfigLoad( path: string, silent = false ) {
         throw new Error( `'${ path }' empty or not loaded` );
     }
 
-    if ( config.inputDtsPath && ! config.outputDtsPath ) {
-        throw new Error( `'${ path }' inputDtsPath is defined but outputDtsPath is not` );
+    let configType: | "single" | "multi" | "unknown" = "unknown";
+
+    // Determine which config type is it.
+
+    // - If first level has all `Z_CONFIG_REQUIRED_KEYS` then it's a single config object.
+    if ( hasRequiredKeys( config ) ) {
+        configType = "single";
+    }
+    // - If one of the second level has all `Z_CONFIG_REQUIRED_KEYS` then it's a multi config object.
+    else if ( Object.values( config ).some( i => hasRequiredKeys( i as IZConfigInternal ) ) ) {
+        configType = "multi";
+    } else {
+        throw new Error( `Invalid config: 'file://${ path }' unable to determine the config type, the required keys are missing:\n` +
+            Object.keys( Z_CONFIG_REQUIRED_KEYS ).join( ", " )
+        );
     }
 
-    config.path = path;
+    const configs: IZConfigInternal[] = [];
 
-    return config as IZConfig;
+    let $defaults: IConfigArgsBase | undefined;
+
+    if ( configType === "single" ) {
+        configEnsureInternals( config as IZConfigInternal, {
+            path,
+            type: configType,
+            name: config.outputName,
+            $defaults,
+        } );
+
+        configValidate( config );
+
+        configs.push( config as IZConfigInternal );
+    } else {
+        Object.entries( config as IZConfigs ).forEach( ( [ key, i ] ) => {
+            if ( i && ! hasRequiredKeys( i as IZConfigInternal ) ) {
+                // TODO: `$defaults` should be const.
+                if ( "$defaults" === key ) {
+                    $defaults = i as IConfigArgsBase;
+                } else {
+                    throw new Error( `Invalid config: 'file://${ path }' the required keys are missing:\n` +
+                        Object.keys( Z_CONFIG_REQUIRED_KEYS ).join( ", " )
+                    );
+                }
+
+                return;
+            }
+
+            configEnsureInternals( i as IZConfigInternal, {
+                path,
+                type: configType,
+                name: key,
+                $defaults,
+            } );
+
+            configValidate( i as IZConfigInternal );
+
+            configs.push( i as IZConfigInternal );
+        } );
+    }
+
+    return configs;
 }
--- packages/zenflux-cli/src/definitions/config.ts
+++ packages/zenflux-cli/src/definitions/config.ts
@@ -2,32 +2,114 @@
  * @author: Leonid Vinikov <leonidvinikov@gmail.com>
  */
 import type { TZFormatType } from "@zenflux/cli/src/definitions/zenflux";
+import type { TForceEnumKeys } from "@zenflux/cli/src/utils/common";
 
-// The arguments are precisely the only things that are used in `zRollupGetConfig`
-// `zRollupGetConfig` runs for each format in the `ZConfig`, those are representing the arguments for each format.
-// Why? Since `zRollupGetConfig` can be called programmatically.
-export interface IConfigArgs {
-    extensions: string[],
-    external?: string[] | RegExp[],
-    format: TZFormatType
-    globals?: { [ key: string ]: string },
+interface IConfigRequiredArgs {
     inputPath: string,
     outputFileName: string,
-    outputName: string,
 }
 
-// Represents `zenflux.config.ts` file, extends `IConfigArgs` with additional properties.
-export interface IZConfig extends Omit<IConfigArgs, "format"> {
-    format: TZFormatType [];
+interface IConfigOptionalArgs {
+    format?: TZFormatType[],
+    extensions?: string[],
 
+    external?: string[] | RegExp[],
+    globals?: {
+        [ key: string ]: string
+    },
+};
+
+// Here are properties that are not format dependent
+interface IConfigArgsGeneral {
+    // TODO: For while `moduleForwarding` will applies for all formats
+    moduleForwarding?: {
+        [ forModule: string ]: {
+            [ source: string ]: string
+        };
+    }
+
+    // Dts probably built once, since it's not format dependent
     inputDtsPath?: string;
     outputDtsPath?: string;
 
     onBuiltFormat?: ( format: TZFormatType | undefined ) => void;
     onBuilt?: () => void;
-
-    /**
-     * @internal
-     */
-    readonly path?: string;
 }
+
+interface IConfigArgsForEachFormat extends IConfigRequiredArgs, IConfigOptionalArgs {
+};
+
+export interface IConfigArgsBase extends IConfigOptionalArgs, IConfigArgsGeneral {
+};
+
+export type TConfigType = "single" | "multi" | "unknown";
+
+/**
+ * @public
+ */
+export interface IZConfig extends IConfigArgsForEachFormat, IConfigArgsGeneral {
+    outputName: string,
+};
+
+/**
+ * @public
+ */
+export interface IZConfigs {
+    /**
+     * Every config object will inherit from this object.
+     */
+    $defaults?: IConfigArgsBase,
+
+    // TODO: add $overrides
+    // TODO: add $rollupOverrides
+
+    [ key: string ]: IZConfig | IConfigArgsBase | undefined,
+};
+
+/**
+ * @internal
+ */
+export interface IZConfigInternal extends IZConfig {
+    type: TConfigType,
+
+    // Path of self.
+    path: string;
+
+    // Required in internal use.
+    format: TZFormatType[],
+
+    // In multi config file it represent as key.
+    outputName: string,
+}
+
+/**
+ * @internal
+ */
+export interface IZConfigArgsRequiredInternal {
+    extensions: string[],
+}
+
+/**
+ * Represents a single configuration object for each format.
+ *
+ * @internal
+ */
+export type TZConfigInternalArgs = Omit<IZConfigInternal, "format"> &
+    Required<IZConfigArgsRequiredInternal> & {
+    format: TZFormatType;
+}
+
+/**
+ * @internal$
+ */
+export const Z_CONFIG_DEFAULTS: IZConfigArgsRequiredInternal = {
+    extensions: [ ".ts", ".js" ],
+};
+
+/**
+ * @internal
+ */
+export const Z_CONFIG_REQUIRED_KEYS = Object.keys({
+    inputPath: true,
+    outputFileName: true,
+} as TForceEnumKeys<IConfigRequiredArgs> );
