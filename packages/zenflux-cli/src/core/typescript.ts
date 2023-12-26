78a675eb2a59e067bc295566c677b27029577014
Leonid Vinikov <czf.leo123@gmail.com>
2023-12-26 18:52:39 +0200

    Tweak: (`z-cli`) - Improve TypeScript diagnostics and validation (multi-threading)
    Introduced validation caching to TypeScript pre-diagnostics to prevent redundant operations. The zTSConfigGetPath function has also been refactored for a more efficient file existence check and streamlined TypeScript configuration file retrieval. Moreover, additional thread support has been provided for diagnostic operations to optimize performance.

--- packages/zenflux-cli/src/core/typescript.ts
+++ packages/zenflux-cli/src/core/typescript.ts
@@ -6,15 +6,24 @@ import process from "node:process";
 import path from "node:path";
 import util from "node:util";
 
+import { workerData, Worker, parentPort } from "node:worker_threads";
+
+import { fileURLToPath, pathToFileURL } from "node:url";
+
 import ts from "typescript";
 
 import { console } from "@zenflux/cli/src/modules/console";
 
+import type { TZPreDiagnosticsArgs } from "@zenflux/cli/src/definitions/typescript";
 import type { TZFormatType } from "@zenflux/cli/src/definitions/zenflux";
+
+const workers = new Map<number, Worker>();
 
 const pathsCache: { [ key: string ]: string } = {},
     configCache: { [ key: string ]: ts.ParsedCommandLine } = {},
-    configValidationCache: { [ key: string ]: boolean } = {};
+    configValidationCache: { [ key: string ]: boolean } = {},
+    cacheCompilerHost: { [ key: string ]: ts.CompilerHost } = {},
+    cacheProgram: { [ key: string ]: ts.Program } = {};
 
 export function zCustomizeDiagnostic( diagnostic: ts.Diagnostic ) {
     let isIntroduceFile = false;
@@ -51,50 +60,64 @@ export function zCustomizeDiagnostic( diagnostic: ts.Diagnostic ) {
 /**
  * Function zTSConfigGetPath() - This function returns the path to the TypeScript configuration file based on the specified format.
  * It checks for different TypeScript configuration files depending on the environment and format.
+ *
+ * Fallback order:
+ * -. tsconfig.{format}.dev.json
+ * -. tsconfig.dev.json
+ * -. tsconfig.{format}.json
+ * -. tsconfig.json
  */
 export function zTSConfigGetPath( format: TZFormatType | null, targetPath: string, showErrors = true ) {
-    const cacheKey = targetPath + "_" + format;
+    function generateCacheKey( format: TZFormatType | null, targetPath: string ): string {
+        return `${ targetPath }_${ format }`;
+    };
+
+    function logVerbose( message: string ) {
+        console.verbose( () => `${ zTSConfigGetPath.name }() -> ${ message }` );
+    };
+
+    function fileExists( filePath: string ) {
+        logVerbose( `'${ filePath }' file exists check` );
+        const exists = fs.existsSync( filePath );
+        logVerbose( `'${ filePath }' ${ exists ? "found" : "not found" }` );
+        return exists;
+    };
+
+    function getTSConfigPath( configFileName: string ) {
+        return path.join( targetPath, configFileName );
+    }
+
+    function getTSConfigBundlePath( source = getTSConfigPath ) {
+        if ( process.env.NODE_ENV === "development" ) {
+            const devFormatFilePath = source( `tsconfig.${ format }.dev.json` );
+            if ( fileExists( devFormatFilePath ) ) {
+                return devFormatFilePath;
+            }
+
+            const devFilePath = source( "tsconfig.dev.json" );
+            if ( fileExists( devFilePath ) ) {
+                return devFilePath;
+            }
+        }
+
+        const formatFilePath = source( `tsconfig.${ format }.json` );
+        if ( fileExists( formatFilePath ) ) {
+            return formatFilePath;
+        }
+
+        const defaultFilePath = source( "tsconfig.json" );
+        if ( fileExists( defaultFilePath ) ) {
+            return defaultFilePath;
+        }
+    }
+
+    const cacheKey = generateCacheKey( format, targetPath );
 
     if ( pathsCache[ cacheKey ] ) {
         return pathsCache[ cacheKey ];
     }
 
-    const targetExist = ( path: string ) => {
-        console.verbose( () => `${ zTSConfigGetPath.name }() -> Checking if '${ path }' file exists` );
-
-        const result = fs.existsSync( path );
-
-        console.verbose( () => `${ zTSConfigGetPath.name }() -> '${ path }' ${ result ? "found" : "not found" }` );
-
-        return result;
-    };
-
-    const getPath = ( targetPath: string ) => {
-        if ( "development" === process.env.NODE_ENV ) {
-            const tsConfigDevFormatPath = path.join( targetPath, `tsconfig.${ format }.dev.json` ),
-                tsConfigDevPath = path.join( targetPath, "tsconfig.dev.json" );
-
-            if ( targetExist( tsConfigDevFormatPath ) ) {
-                return tsConfigDevFormatPath;
-            } else if ( targetExist( tsConfigDevPath ) ) {
-                return tsConfigDevPath;
-            }
-        }
-
-        const tsConfigFormatPath = path.join( targetPath, `tsconfig.${ format }.json` );
-
-        if ( targetExist( tsConfigFormatPath ) ) {
-            return tsConfigFormatPath;
-        }
-
-        const tsConfigPath = path.join( targetPath, "tsconfig.json" );
-
-        if ( targetExist( tsConfigPath ) ) {
-            return tsConfigPath;
-        }
-    };
-
-    const tsConfigPath = getPath( targetPath );
+    const tsConfigPath = getTSConfigBundlePath();
 
     if ( tsConfigPath ) {
         return pathsCache[ cacheKey ] = tsConfigPath;
@@ -170,12 +193,49 @@ export function zTSConfigRead( format: TZFormatType | null, projectPath: string 
 }
 
 /**
+ * Function zTSGetCompilerHost() - Retrieves the compiler host for a given TypeScript configuration.
+ *
+ * Retrieves the compiler host for a given TypeScript configuration.
+ *
+ * @param {ts.ParsedCommandLine} tsConfig - The TypeScript configuration.
+ */
+export function zTSGetCompilerHost( tsConfig: ts.ParsedCommandLine ) {
+    const declarationPath = tsConfig.options.declarationDir || tsConfig.options.outDir;
+
+    if ( ! declarationPath ) {
+        throw new Error( `${ tsConfig.options.configFilePath }: 'declarationDir' or 'outDir' is required` );
+    }
+
+    // Get from cache
+    if ( cacheCompilerHost[ tsConfig.options.configFilePath as string ] ) {
+        return cacheCompilerHost[ tsConfig.options.configFilePath as string ];
+    }
+
+    const compilerHost = ts.createCompilerHost( tsConfig.options, true ),
+        compilerHostGetSourceFile = compilerHost.getSourceFile;
+
+    compilerHost.getSourceFile = ( fileName, languageVersion, onError, shouldCreateNewSourceFile ) => {
+        // Exclude internal TypeScript files from validation
+        if ( fileName.startsWith( declarationPath ) ) {
+            console.verbose( () => `${ zTSPreDiagnostics.name }() -> Skipping validation for '${ fileName }', internal TypeScript file` );
+            return;
+        }
+
+        return compilerHostGetSourceFile( fileName, languageVersion, onError, shouldCreateNewSourceFile );
+    };
+
+    cacheCompilerHost[ tsConfig.options.configFilePath as string ] = compilerHost;
+
+    return compilerHost;
+}
+
+/**
  * Function zTSPreDiagnostics() - Runs pre-diagnostics for specific TypeScript configuration.
  */
-export function zTSPreDiagnostics( tsConfig: ts.ParsedCommandLine, args: {
-    useCache?: boolean,
-    haltOnError?: boolean,
-} = {} ) {
+export function zTSPreDiagnostics( tsConfig: ts.ParsedCommandLine, args: TZPreDiagnosticsArgs = {} ) {
+    if ( args.thread || 0 === args.thread ) {
+        return zTSCreateDiagnosticWorker( tsConfig, args );
+    }
     // ---
     const {
         useCache = true,
@@ -191,24 +251,7 @@ export function zTSPreDiagnostics( tsConfig: ts.ParsedCommandLine, args: {
         return;
     }
 
-    const declarationPath = tsConfig.options.declarationDir || tsConfig.options.outDir;
-
-    if ( ! declarationPath ) {
-        throw new Error( `${ tsConfig.options.configFilePath }: 'declarationDir' or 'outDir' is required` );
-    }
-
-    const compilerHost = ts.createCompilerHost( tsConfig.options, true ),
-        compilerHostGetSourceFile = compilerHost.getSourceFile;
-
-    compilerHost.getSourceFile = ( fileName, languageVersion, onError, shouldCreateNewSourceFile ) => {
-        // Exclude internal TypeScript files from validation
-        if ( fileName.startsWith( declarationPath) ) {
-            console.verbose( () => `${ zTSPreDiagnostics.name }() -> Skipping validation for '${ fileName }', internal TypeScript file` );
-            return;
-        }
-
-        return compilerHostGetSourceFile( fileName, languageVersion, onError, shouldCreateNewSourceFile );
-    };
+    const compilerHost = zTSGetCompilerHost( tsConfig );
 
     const program = ts.createProgram( tsConfig.fileNames, Object.assign( {}, tsConfig.options, {
         noEmit: true,
@@ -220,7 +263,9 @@ export function zTSPreDiagnostics( tsConfig: ts.ParsedCommandLine, args: {
 
         declaration: false,
         declarationMap: false,
-    } as ts.CompilerOptions ), compilerHost );
+    } as ts.CompilerOptions ), compilerHost, cacheProgram[ tsConfig.options.configFilePath as string ] );
+
+    cacheProgram[ tsConfig.options.configFilePath as string ] = program;
 
     const diagnostics = ts.getPreEmitDiagnostics( program );
 
@@ -239,7 +284,7 @@ export function zTSPreDiagnostics( tsConfig: ts.ParsedCommandLine, args: {
         }
     }
 
-    return diagnostics.length;
+    return diagnostics.length <= 0;
 }
 
 export function zTSCreateDeclaration( tsConfig: ts.ParsedCommandLine ) {
@@ -248,7 +293,9 @@ export function zTSCreateDeclaration( tsConfig: ts.ParsedCommandLine ) {
         noEmit: false,
         emitDeclarationOnly: true,
         declarationDir: tsConfig.options.declarationDir || tsConfig.options.outDir,
-    } as ts.CompilerOptions ) );
+    } as ts.CompilerOptions ), zTSGetCompilerHost( tsConfig ), cacheProgram[ tsConfig.options.configFilePath as string ] );
+
+    cacheProgram[ tsConfig.options.configFilePath as string ] = program;
 
     // Remove old declaration .d.ts files
     const declarationPath = tsConfig.options.declarationDir || tsConfig.options.outDir;
@@ -273,4 +320,98 @@ export function zTSCreateDeclaration( tsConfig: ts.ParsedCommandLine ) {
     return program;
 }
 
+function zTSDiagnosticInWorker() {
+    if ( ! workerData.args.thread && 0 !== workerData.args.thread ) {
+        throw new Error( "Thread options not found." );
+    }
+
+    if ( ! workerData.tsConfigPath ) {
+        throw new Error( "tsConfigPath not found." );
+    }
+
+    if ( ! workerData.args ) {
+        throw new Error( "args not found." );
+    }
+
+    const id = workerData.args.thread as number,
+        tsConfig = zTSConfigRead( null, path.dirname( workerData.tsConfigPath ) );
+
+    // Waiting for the parent to send a message.
+    parentPort?.on( "message", async ( message ) => {
+        switch ( message ) {
+            case "run":
+                console.verbose( () => `Thread\t${ id }\tRun\t${ util.inspect( tsConfig.options.configFilePath ) }` );
+
+                const result = zTSPreDiagnostics( tsConfig, {
+                    ... workerData.args,
+                    thread: false,
+                } );
+
+                // Ensuring that console.log is flushed.
+                setTimeout( () => {
+                    parentPort?.postMessage( [
+                        "done",
+                        result,
+                    ] );
+                } );
+
+                break;
+
+            default:
+                throw new Error( `Unknown message: ${ message }` );
+        }
+    } );
+}
+
+export async function zTSCreateDiagnosticWorker( tsConfig: ts.ParsedCommandLine, args: TZPreDiagnosticsArgs = {} ) {
+    // Since worker do not need to load it, it can't be in top level.
+    const { zGlobalPathsGet } = ( await import( "@zenflux/cli/src/core/global" ) );
+
+    // Await for the worker to finish.
+    return new Promise( async ( resolve ) => {
+        if ( ! workers.has( args.thread as number ) ) {
+            console.verbose( () => `Diagnostic\t${ args.thread }\tStart\t${ util.inspect( tsConfig.options.configFilePath ) }` );
+
+            const worker = new Worker( pathToFileURL( zGlobalPathsGet().cli ), {
+                name: `zTSCreateDiagnosticWorker-${ args.thread }`,
+                argv: process.argv,
+                workerData: {
+                    zCliWorkPath: fileURLToPath( import.meta.url ),
+
+                    args,
+
+                    tsConfigPath: tsConfig.options.configFilePath,
+
+                    action: "tsDiagnostic",
+                },
+            } );
+
+            workers.set( args.thread as number, worker );
+        }
+
+        const worker = workers.get( args.thread as number ) as Worker;
+
+        worker.once( "message", ( [ action, result ] ) => {
+            switch ( action ) {
+                case "done":
+                    resolve( result as boolean );
+                    break;
+
+                default:
+                    throw new Error( `Unknown action: ${ action }` );
+        } } );
+
+        worker.postMessage( "run" );
+    } );
+}
+
+if ( workerData?.zCliWorkPath === fileURLToPath( import.meta.url ) ) {
+    switch ( workerData.action ) {
+        case "tsDiagnostic":
+            zTSDiagnosticInWorker();
+            break;
+
+        default:
+            throw new Error( `Unknown action: ${ workerData.action }` );
+    }
 }
---
+++ packages/zenflux-cli/src/definitions/typescript.ts
@@ -0,0 +1,5 @@
+export type TZPreDiagnosticsArgs = {
+    useCache?: boolean,
+    haltOnError?: boolean,
+    thread?: number,
+}
