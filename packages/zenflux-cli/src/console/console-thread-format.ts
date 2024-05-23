af108b578d67ae8d8ed3b70a7117063fff71db01
Leonid Vinikov <czf.leo123@gmail.com>
2024-05-23 07:18:07 +0300

    Tweak: (`zenflux-cli`) - Add docstrings to `ConsoleThreadFormat` in `console-thread-format.ts`
    Detailed docs were added to the `ConsoleThreadFormat` class in `console-thread-format.ts`. They provide information on the expected output of several abstract methods, the class's purpose, and the behavior of the constructor function. This enhances code clarity and promotes proper implementation of the class methods.

--- packages/zenflux-cli/src/console/console-thread-format.ts
+++ packages/zenflux-cli/src/console/console-thread-format.ts
@@ -1,3 +1,6 @@
+/**
+ * @author: Leonid Vinikov <leonidvinikov@gmail.com>
+ */
 import process from "node:process";
 
 import { ConsoleManager } from "@zenflux/cli/src/managers/console-manager";
@@ -7,7 +10,15 @@ import type { TConsoleLoggerMethod } from "@zenflux/cli/src/modules/console/cons
 const stdout = process.stdout,
     stderr = process.stderr;
 
+/**
+ * ConsoleThreadFormat is an abstract class that provides a structure for formatting console threads.
+ * It also provides the ability to switch formatting based on logging level.
+ */
 export abstract class ConsoleThreadFormat extends ConsoleManager.module() {
+    /**
+     * The constructor checks if the getFormatExtended method is defined and if either the verbose or debug flags are enabled.
+     * If these conditions are met, it replaces the getFormat method with the getFormatExtended method.
+     */
     public constructor() {
         super( {
             stdout,
@@ -19,14 +30,26 @@ export abstract class ConsoleThreadFormat extends ConsoleManager.module() {
         }
     }
 
+    /**
+     * Abstract method that should return the name of the thread.
+     * This method is expected to be implemented by any class that extends ConsoleThreadFormat.
+     */
     public abstract getName(): string;
 
+    /**
+     * Abstract method that should return the ID of the thread.
+     * This method is expected to be implemented by any class that extends ConsoleThreadFormat.
+     */
     public abstract getThreadId(): number|string;
 
+    /**
+     * Abstract method that should return the code of the thread.
+     * This method is expected to be implemented by any class that extends ConsoleThreadFormat.
+     */
     public abstract getThreadCode(): string;
 
     /**
-     * Function getFormatExtended(): Give the ability to set different format while verbose or debug is enabled.
+     * Optional method that, if implemented in a subclass, should return a string that represents a different format for verbose or debug logging.
      */
     protected getFormatExtended?( method: TConsoleLoggerMethod, args: any[] ): string;
 }
