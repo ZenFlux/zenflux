441466d732e8f22910c5f32c8d6ab9a7f124db74
Leonid Vinikov <czf.leo123@gmail.com>
2024-09-30 14:12:36 +0300

    Remove: (`zenflux-cli`) - Unused `.js` dist files

--- packages/zenflux-cli/src/boot.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * @author: Leonid Vinikov <leonidvinikov@gmail.com>
- */
-var index_1 = require("@zenflux/cli/src/index");
-await (0, index_1.default)();
