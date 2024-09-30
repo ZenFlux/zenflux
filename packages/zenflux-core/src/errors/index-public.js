a68cd4f62de92fa35919fe9289159ed671248041
Leonid Vinikov <czf.leo123@gmail.com>
2024-09-30 07:23:41 +0300

    Remove: (`zenflux-core`) - Unused `.js` dist files

--- packages/zenflux-core/src/errors/index-public.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ForceMethodImplementation = void 0;
-var force_method_implementation_1 = require("@z-core/errors/force-method-implementation");
-Object.defineProperty(exports, "ForceMethodImplementation", { enumerable: true, get: function () { return force_method_implementation_1.ForceMethodImplementation; } });
--- packages/zenflux-core/src/interfaces/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/**
- * @author Leonid Vinikov <leonidvinikov@gmail.com>
- */
-Object.defineProperty(exports, "__esModule", { value: true });
