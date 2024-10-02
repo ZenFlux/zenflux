28cc7fc25b1e7735eddc85a6aada1a09a31e02b9
Leonid Vinikov <czf.leo123@gmail.com>
2024-10-02 18:12:33 +0300

    wip

--- packages/zenflux-cli/src/index.ts
+++ packages/zenflux-cli/src/index.ts
@@ -6,7 +6,11 @@ import process from "node:process";
 
 import { ConsoleManager } from "@zenflux/cli/src/managers/console-manager";
 
+import packageJSON from "@zenflux/cli/package.json" assert { type: "json" };
+
 export default async function boot( args = process.argv.slice( 2 ) ) {
+    ConsoleManager.$.log( `(${ packageJSON.name }~v${ packageJSON.version }) Starting...`);
+
     const commands = {
         "@watch": {
             module: async () => ( await import( "@zenflux/cli/src/commands/watch" ) ).default,
