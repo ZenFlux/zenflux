82b053fd70a60eccf238ba15a0b9f2db7e81d555
Leonid Vinikov <czf.leo123@gmail.com>
2024-10-04 20:42:18 +0300

    New: (`zenflux-typescript-vm`) - Add memory usage verbose mode with `--zvm-memory-verbose` flag
    Introduced a new feature to measure and log memory usage in different modes based on the `--zvm-memory-verbose` flag. This includes isolated and detailed memory usage statistics, printed periodically depending on the mode selected.

--- packages/zenflux-typescript-vm/src/index.js
+++ packages/zenflux-typescript-vm/src/index.js
@@ -177,6 +177,52 @@ const initialize = async () => {
      * @param {Resolvers} resolvers
      */
     function auto( entrypointPath, loaders, resolvers ) {
+        const argIndex = process.argv.findIndex( a => a === "--zvm-memory-verbose" );
+
+        if  ( argIndex !== -1 ) {
+            let mode = process.argv[ argIndex + 1 ];
+
+            switch( mode ) {
+                case "isolated": {
+                    mode = "summary"
+                }
+                break;
+
+                default:
+                case "total":
+                    mode = "detailed";
+                    break;
+            }
+
+            const printMemoryUsage = () =>{
+                /**
+                 * @typedef {ReturnType<typeof import("node:vm").measureMemory>} ResultPromise
+                 */
+                measureMemory( {
+                    mode,
+                    execution: "eager"
+                } ).then( ( result ) => {
+                    /**
+                     * @type {Awaited<ResultPromise>}
+                     */
+                    const usage = result;
+
+                    // Convert to MB.
+                    const formated  = {
+                        estimate: ( Math.round( usage.total.jsMemoryEstimate / 1024 / 1024 * 100 ) / 100  ) + "MB",
+                        low: ( Math.round( usage.total.jsMemoryRange[ 0 ] / 1024 / 1024 * 100 ) / 100  ) + "MB",
+                        high: ( Math.round( usage.total.jsMemoryRange[ 1 ] / 1024 / 1024 * 100 ) / 100  ) + "MB",
+                    };
+
+                    console.log( "--zvm-memory-verbose with mode:", mode, util.inspect( formated ) );
+                } );
+            };
+
+            printMemoryUsage();
+
+            setInterval( printMemoryUsage, mode === "summary" ? 10000 : 1000 );
+        }
+
         // defer to next tick, allow to load all providers.
         return new Promise( ( resolve, reject ) => {
             setTimeout( () => {
