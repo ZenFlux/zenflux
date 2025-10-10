54ddb4ba188bba86d6850352ed83d917e2103b61
Leonid Vinikov <czf.leo123@gmail.com>
2025-10-10 18:16:02 +0300

    Fix: (`zenflux-cli`) - Fixes import meta URL handling
    Ensures import meta URL is correctly handled in different environments.
    
    It avoids potential issues where the URL might be undefined. This change also ensures referer URL is correctly assigned.

--- packages/zenflux-typescript-vm/src/loaders.js
+++ packages/zenflux-typescript-vm/src/loaders.js
@@ -245,14 +245,16 @@ export class Loaders {
 
                 if ( ! sourceModuleOptions.initializeImportMeta ) {
                     sourceModuleOptions.initializeImportMeta = ( meta, module ) => {
-                        meta.url = path.startsWith( "file://" ) ? path : "file://" + path;
+                        if ( meta ) {
+                            meta.url = path.startsWith( "file://" ) ? path : "file://" + path;
 
-                        const referer = options.moduleLocalTextSourceOptions?.referencingModule?.identifier;
+                            const referer = options.moduleLocalTextSourceOptions?.referencingModule?.identifier;
 
-                        if ( referer ) {
-                            meta.refererUrl = referer.startsWith( "file://" ) ? referer : "file://" + referer;
+                            if ( referer ) {
+                                meta.refererUrl = referer.startsWith( "file://" ) ? referer : "file://" + referer;
 
-                            this.moduleRefererCache.set( meta.url, meta.refererUrl );
+                                this.moduleRefererCache.set( meta.url, meta.refererUrl );
+                            }
                         }
                     };
                 }
