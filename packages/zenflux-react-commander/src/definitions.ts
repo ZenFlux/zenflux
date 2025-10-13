f0532bcb1a4def69e0c7ccf2dbf4c5eeb3627a93
Leonid Vinikov <czf.leo123@gmail.com>
2025-10-13 12:18:39 +0300

    tweak: (`zenflux-react-commander`) - update event emitter import to eventemitter3

--- packages/zenflux-react-commander/src/definitions.ts
+++ packages/zenflux-react-commander/src/definitions.ts
@@ -1,6 +1,6 @@
 import type React from "react";
 
-import type { EventEmitter } from "events";
+import type EventEmitter from "eventemitter3";
 
 import type { CommandBase } from "@zenflux/react-commander/command-base";
 
